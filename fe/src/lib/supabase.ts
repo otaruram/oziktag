257fa0e2916c786c4624bf784dfb359f72d1d7c5
otaruram <otaruna61@gmail.com>
2026-08-13 22:44:07 +0700

    chore: move supabase keys to env vars

--- fe/src/lib/supabase.ts
+++ fe/src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://onxsbnsmcluyexwkrawx.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueHNibnNtY2x1eWV4d2tyYXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NDY5OTUsImV4cCI6MjA5NjMyMjk5NX0.qqD_fsev8s-5_QjxRJPLeVOn9aldNbxBJl1W-N36-uM";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
